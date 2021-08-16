3a91c3ce937c026f2ab08180e9d80753c447f968
Gm Awesome <gmmoose17@gmail.com>
2021-08-16 20:57:38 +0800

    ✨ updated gulpfile

--- stats-preview-card-component/gulpfile.js
+++ stats-preview-card-component/gulpfile.js
@@ -47,36 +47,3 @@ function watchTask() {
 
 // Default Gulp Task
 exports.default = series(scssTask, browserSyncServe, watchTask);
-
-// const gulp = require("gulp");
-// const sass = require("gulp-sass")(require("sass"));
-// const browserSync = require("browser-sync").create();
-
-// // compile scss into css
-// function style() {
-//   // 1. where is my scss file
-//   return (
-//     gulp
-//       .src("./scss/**/*.scss")
-//       // 2. pass that file through sass compiler
-//       .pipe(sass())
-//       // 3. where do I save the compiled css?
-//       .pipe(gulp.dest("./css"))
-//       // 4. stream changes to all browsers
-//       .pipe(browserSync.stream())
-//   );
-// }
-
-// function watch() {
-//   browserSync.init({
-//     server: {
-//       baseDir: "./",
-//     },
-//   });
-//   gulp.watch("./scss/**/*.scss", style);
-//   gulp.watch("./*.html").on("change", browserSync.reload);
-//   // gulp.watch('./scss/**/*.scss')
-// }
-
-// exports.style = style;
-// exports.watch = watch;
